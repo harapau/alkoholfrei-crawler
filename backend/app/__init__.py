78d595436040dadad5fbb94e34e640c422d5b7bc
unknown <paustian.harald@gmail.com>
2026-06-11 21:33:14 +0200

    Frontend index.html korrekt eingebungen über routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,7 +9,10 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    templates_path = os.path.join(base_dir, 'frontend', 'templates')
+    static_path = os.path.join(base_dir, 'frontend', 'static')
+    app = Flask(__name__, template_folder=templates_path, static_folder=static_path)
     app.config.from_object(config_class)
 
     # Initialisiere die Datenbank
@@ -21,7 +26,9 @@ def create_app(config_class=Config):
     # Blueprints registrieren
     from app.routes.admin import bp as admin_bp
     from app.routes.api import bp as api_bp
+    from app.routes.public import bp as public_bp
 
+    app.register_blueprint(public_bp, url_prefix=None)  # Öffentliche Routen ohne Präfix
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(api_bp, url_prefix='/api')
 
